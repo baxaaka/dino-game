72d4a723b510f640ad7e3b1d1eef0495e61e7126
baxaaka <jockuhacker@gmail.com>
2023-02-02 21:19:16 +0500

    win

--- script.js
+++ script.js
@@ -1,550 +1,404 @@
-
 const box = document.querySelector(".box");
-
 
 const def = document.querySelector(".def");
 
-
-
-
 const section = document.querySelector(".section");
 
- const input = document.querySelector("input");
-
+const input = document.querySelector("input");
+const bld = document.querySelector(".bld")
 
 input.value = 0;
 
-
-
- 
 // ////////////////////////////////////////////
- 
 
-let addRun= 0
+let addRun = 0;
 
-function run(){
-  if(dispatchEvent.classList != "run"){
+function run() {
+  if (dispatchEvent.classList != "run") {
+    addRun -= 10;
 
-    addRun -= 10  
+    box.classList.add("run");
+    box.classList.remove("def");
 
-    box.classList.add("run")
-    box.classList.remove("def")
-  
-    box.style.right = addRun + "px"
- 
+    box.style.right = addRun + "px";
 
-    if(input.value> 0 && input.value >20){
+    if (input.value > 0 && input.value > 20) {
       // alert("yesss")
-
-        // location.reload()
-     
-     
+      // location.reload()
     }
 
- 
     // setTimeout(function() {
-    
-    
-    input.value++
-      
-    
+
+    input.value++;
+
     // }, 5000);
   }
 }
 
-
-function runStop(){
-  box.classList.remove("run")
-  box.classList.add("def")
-
-
-
- 
-  
-
-
+function runStop() {
+  box.classList.remove("run");
+  box.classList.add("def");
 }
 
+let addRun2 = 0;
 
+function runLeft() {
+  if (dispatchEvent.classList != "runLeft") {
+    addRun2 -= 10;
 
+    box.classList.add("runLeft");
+    box.classList.remove("defLeft");
+    box.classList.remove("def");
 
-
-let addRun2= 0
-
-function runLeft(){
-  if(dispatchEvent.classList != "runLeft"){
-
-    addRun2 -= 10    
-
-    box.classList.add("runLeft")
-    box.classList.remove("defLeft")
-    box.classList.remove("def")
-  
-    box.style.left = addRun2 + "px"
- 
-  
+    box.style.left = addRun2 + "px";
   }
 }
 
-
-function runLeftStop(){
-  box.classList.remove("runLeft")
-  box.classList.add("defLeft")
-
-
-
- 
-  
-
-
+function runLeftStop() {
+  box.classList.remove("runLeft");
+  box.classList.add("defLeft");
 }
 
+let tpp = 0;
 
+function topRun() {
+  tpp -= 1;
 
-let tpp = 0
-
-function topRun(){
-  tpp -=1
-
-  box.style.top=tpp +"px"
+  box.style.top = tpp + "px";
 }
 
-function jump(){
-  
-  if(dispatchEvent.classList != "jump"){
-    box.classList.add("jump")
-    
+function jump() {
+  if (dispatchEvent.classList != "jump") {
+    box.classList.add("jump");
   }
 
- 
-
-  if(input.value> 0 && input.value >17){
-    box.style.top ="80px"
-    box.style.left= "100px"
-
- 
-}
+  if (input.value > 0 && input.value > 17) {
+    box.style.top = "80px";
+    box.style.left = "100px";
+  }
 
   setTimeout(() => {
-    box.classList.remove("jump")
-    
+    box.classList.remove("jump");
   }, 200);
+}
 
-
- }
-
- 
-function trass(){
-  if(dispatchEvent.classList != "trass"){
-    section.classList.add("trass")
-   
-  
-  
+function trass() {
+  if (dispatchEvent.classList != "trass") {
+    section.classList.add("trass");
   }
 }
 
-
-function trassStop(){
-  setTimeout(function() {
-    
-    section.classList.remove("trass")
-  
+function trassStop() {
+  setTimeout(function () {
+    section.classList.remove("trass");
   }, 300);
 }
 
-
-
-function attack(){
-  if(dispatchEvent.classList != "attack"){
-    box.classList.add("attack")
+function attack() {
+  if (dispatchEvent.classList != "attack") {
+    box.classList.add("attack");
     setTimeout(() => {
-      box.classList.remove("attack")
-      
+      box.classList.remove("attack");
     }, 600);
-  
   }
-
-
-
 }
 
-
-
-function attack2(){
-  if(dispatchEvent.classList != "attack2"){
-    box.classList.add("attack2")
+function attack2() {
+  if (dispatchEvent.classList != "attack2") {
+    box.classList.add("attack2");
     setTimeout(() => {
-      box.classList.remove("attack2")
-      
+      box.classList.remove("attack2");
     }, 600);
-  
   }
-
-
-
 }
 
-function jumpAtack(){
-  if(dispatchEvent.classList != "jumpAtack"){
-    box.classList.add("jumpAtack")
-    
+function jumpAtack() {
+  if (dispatchEvent.classList != "jumpAtack") {
+    box.classList.add("jumpAtack");
   }
   setTimeout(() => {
-    box.classList.remove("jumpAtack")
-    
+    box.classList.remove("jumpAtack");
   }, 2000);
+}
 
-
- }
-
-
-
-
- 
-let rar = 0
+let rar = 0;
 // let rar2 = 0
 
 // let inn = input.value
 
 //  function gameOver(){
 
-
-   
 //    rar2 -= -200
 
- 
-
-   
 //   }else{
 //     alert("game over")
 
 //   }
 //  }
- 
 
 // gameOver()
 
-
-
-
-
-
-document.addEventListener( "keydown" , function(e){
-  
- 
-
-
-
-   if(e.keyCode == '87' ){
+document.addEventListener("keydown", function (e) {
+  if (e.keyCode == "87") {
     // jump()
-    topRun()
-  }
-  
-  if(e.keyCode == '65'){
-   
-    runLeft()
-    
+    topRun();
   }
 
-  if(e.keyCode == '68' ){
-    run()
+  if (e.keyCode == "65") {
+    runLeft();
   }
-  
+
+  if (e.keyCode == "68") {
+    run();
+  }
+
   // if(e.key == 'ArrowUp' && e.keyCode == '13' ){
   //   jumpAtack()
   //   jump()
   // }
+});
 
-})
+document.addEventListener("dblclick", () => {
+  attack2();
+});
 
-document.addEventListener("dblclick" , ()=>{
-  attack2()
-})
+document.addEventListener("mousedown", () => {
+  attack();
+  sward();
+  trass();
+});
 
-document.addEventListener("mousedown" , ()=>{
-  attack()
-  sward()
-  trass()
- 
+document.addEventListener("mouseup", () => {
+  trassStop();
+});
 
-})
-
-
-document.addEventListener("mouseup" , ()=>{
-
-  
-
-    trassStop()
-    
-  
-})
-
-
-document.addEventListener( "keyup" , function(e){
-  if(e.keyCode == '68'){
-    runStop()
-  
-  }
- 
-  if(e.keyCode == '65'){
-  
-    runLeftStop()
+document.addEventListener("keyup", function (e) {
+  if (e.keyCode == "68") {
+    runStop();
   }
 
-  
- 
-  
- })
+  if (e.keyCode == "65") {
+    runLeftStop();
+  }
+});
 
-
-
-
-
-
-
-
-
- function sward() {
+function sward() {
   var sward = new Audio("./audio/sward.wav");
   sward.volume = "0.1";
   sward.play();
- }
+}
+
+
+
+
 
 
 
 
 //  ------------------------box 2 dushman ---------------------------------------------
 
-let input2 =document.querySelector(".input2")
+
+
+
+
+
+let input2 = document.querySelector(".input2");
 
 input2.value = 0;
 const box2 = document.querySelector(".box2");
 
 const def2 = document.querySelector(".def2");
 
-let addRun11= 0
+let addRun11 = 0;
 
-function run2(){
-  if(dispatchEvent.classList != "run2"){
+function run2() {
+  if (dispatchEvent.classList != "run2") {
+    addRun11 -= 10;
 
-    addRun11 -= 10  
+    box2.classList.add("run2");
+    box2.classList.remove("def2");
 
-    box2.classList.add("run2")
-    box2.classList.remove("def2")
-    
-    box2.style.right = addRun11 + "px"
- 
+    box2.style.left = addRun11 + "px";
 
-    if(input2.value> 0 && input2.value >20){
-      if(box2.style.top ="100px"){
-        alert("yesss")
+    if (input2.value > 0 && input2.value > 20) {
+      alert("yesss");
 
-        // location.reload()
-    
-      }
-     
-     
+      // location.reload()
     }
 
-    input2.value++
-
-
+    input2.value++;
   }
 }
 
-
-function runStop2(){
-  box2.classList.remove("run2")
-  box2.classList.add("def2")
+function runStop2() {
+  box2.classList.remove("run2");
+  box2.classList.add("def2");
 }
 
+let addRun22 = 0;
 
-let addRun22= 0
+function runLeft2() {
+  if (dispatchEvent.classList != "run2") {
+    addRun22 -= 10;
 
-function runLeft2(){
-  if(dispatchEvent.classList != "run"){
+    box2.classList.add("run2");
+    box2.classList.remove("def2");
 
-    addRun22 -= 10    
-
-    box2.classList.add("run2")
-    box2.classList.remove("def")
-  
-    box2.style.right = addRun22 + "px"
- 
- 
+    box2.style.right = addRun22 + "px";
   }
 }
 
-
-function runLeftStop2(){
-  box2.classList.remove("run2")
-  box2.classList.add("def2")
+function runLeftStop2() {
+  box2.classList.remove("run2");
+  box2.classList.add("def2");
 }
 
+// let tpp2 = 0;
 
+// function topRun2() {
+//   tpp2 -= 1;
 
-let tpp2 = 0
+//   box2.style.top = tpp2 + "px";
+// }
 
-function topRun2(){
-  tpp2 -=1
-
-  box2.style.top=tpp2 +"px"
-}
-
-function jump2(){
-  
-  if(dispatchEvent.classList != "jump2"){
-    box2.classList.add("jump2")
-    
+function jump2() {
+  if (dispatchEvent.classList != "jump2") {
+    box2.classList.add("jump2");
   }
 
- 
-
-  if(input2.value> 0 && input2.value >17){
-    box2.style.top ="80px"
-    box2.style.left= "100px"
-
- 
-}
+  // if (input2.value > 0 && input2.value > 17) {
+  //   box2.style.top = "80px";
+  //   box2.style.left = "100px";
+  // }
 
   setTimeout(() => {
-    box2.classList.remove("jump2")
-    
+    box2.classList.remove("jump2");
   }, 200);
+}
 
-
- }
-
- 
-function trass2(){
-  if(dispatchEvent.classList != "trass2"){
-    section.classList.add("trass2")
-   
-  
-  
+function trass2() {
+  if (dispatchEvent.classList != "trass2") {
+    section.classList.add("trass2");
   }
 }
 
-
-function trassStop2(){
-  setTimeout(function() {
-    
-    section.classList.remove("trass2")
-  
+function trassStop2() {
+  setTimeout(function () {
+    section.classList.remove("trass2");
   }, 300);
 }
 
-
-
-function attack22(){
-  if(dispatchEvent.classList != "attack22"){
-    box2.classList.add("attack22")
+function attack22() {
+  if (dispatchEvent.classList != "attack22") {
+    box2.classList.add("attack22");
     setTimeout(() => {
-      box2.classList.remove("attack22")
-      
+      box2.classList.remove("attack22");
     }, 600);
-  
   }
-
-
-
 }
 
-
-
-
-function attack33(){
-  if(dispatchEvent.classList != "attack33"){
-    box2.classList.add("attack33")
+function attack33() {
+  if (dispatchEvent.classList != "attack33") {
+    box2.classList.add("attack33");
     setTimeout(() => {
-      box2.classList.remove("attack33")
-      
+      box2.classList.remove("attack33");
     }, 600);
-  
   }
-
-
-
 }
 
-
-
-
-
-document.addEventListener("dblclick" , ()=>{
-  attack33()
-})
-
-
-document.addEventListener( "keydown" , function(e){
-  
- 
-  if(e.keyCode == '37' ){
-    run2()
-
-  
-
-    // rar -= 5
+function death2() {
+  if (dispatchEvent.classList != "death2") {
+    box2.classList.add("death2");
+    deathAudio2();
    
-    
+   setTimeout(() => {
+    finish()
+
+   }, 500);
+
+   setTimeout(() => {
+    alert("You win")
+    location.reload()
+   }, 1500);
+    setTimeout(() => {
+      box2.style.animationPlayState = "paused";
+    }, 600);
+  }
+}
+
+
+
+function blood11(){
+
+  if (dispatchEvent.classList != "blood11") {
+    bld.classList.add("blood11");
  
   }
+}
 
 
-   if(e.keyCode == '38' ){
+document.addEventListener("dblclick", () => {
+  attack33();
+
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.keyCode == "37") {
+    run2();
+  }
+
+  if (e.keyCode == "38") {
     // jump()
-    topRun2()
-  }
-  
-  if(e.keyCode == '39'){
    
-    runLeft2()
-    
   }
-  
+
+  if (e.keyCode == "39") {
+    runLeft2();
+  }
+
   // if(e.key == 'ArrowUp' && e.keyCode == '13' ){
   //   jumpAtack()
   //   jump()
   // }
+});
 
-})
+document.addEventListener("mousedown", () => {
+  // attack22()
+  blood11()
+  trass2();
+  input2.value++;
 
+  if (input2.value > 0 && input2.value > 5) {
+    death2();
 
-document.addEventListener("mousedown" , ()=>{
-  attack22()
-
-  trass2()
-  // input.value++
-
-
- 
-
-
-})
-
-
-document.addEventListener("mouseup" , ()=>{
-
-    
-    trassStop2()
-    
-  
-})
-
-
-document.addEventListener( "keyup" , function(e){
-  if(e.keyCode == '37'){
-    runStop2()
-  
   }
- 
-  if(e.keyCode == '39'){
-  
-    runLeftStop2()
+  attack22() 
+
+
+});
+
+document.addEventListener("mouseup", () => {
+  trassStop2();
+});
+
+document.addEventListener("keyup", function (e) {
+  if (e.keyCode == "37") {
+    runStop2();
   }
 
-  
+  if (e.keyCode == "39") {
+    runLeftStop2();
+  }
+});
 
-  
- })
+function deathAudio2() {
+  var death2 = new Audio("./audio/death.wav");
+  death2.volume = "0.1";
+  death2.play();
+}
 
+function finish() {
+  var finish = new Audio("./audio/fitality.mp3");
+  finish.volume = "0.6";
+  finish.play();
+}
